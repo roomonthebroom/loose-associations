4399923655b57a8f9cdf15b3f415b127fdf78ed1
Julie <whyawake@gmail.com>
2025-11-30 13:24:19 -0800

    setup: change project name. use game title constant in header. add spacing for info modal trigger.
    spacing was needed for <InfoModal /> since the updates to remove button nesting rendering resulted in this icon not getting standard button styling. mt-4 seems to give it enough centering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
-import React from "react";
-
-import InfoModal from "../modals/InfoModal";
+import React from 'react';
+import InfoModal from '../modals/InfoModal';
+import { GAME_TITLE } from '../../lib/constants';
 
 function Header() {
   return (
     <header>
-      <h1 className="font-space-mono">Black Connections</h1>
-      <InfoModal />
+      <h1 className="font-space-mono">{GAME_TITLE}</h1>
+      <span className="mt-4">
+        <InfoModal />
+      </span>
     </header>
   );
 }
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,2 @@
-export const GAME_TITLE = "Black Connections";
+export const GAME_TITLE = 'Loose Associations';
 export const MAX_MISTAKES = 4;
